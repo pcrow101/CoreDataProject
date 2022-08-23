cbe63ea80cbedd0837e7a09c68e8fd51aa41e87c
Paul Crow <paucrow@synamedia.com>
2022-08-23 11:37:43 +0100

    Initial Commit

---
+++ CoreDataProject/CoreDataProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataProjectApp.swift
+//  CoreDataProject
+//
+//  Created by paucrow on 23/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
